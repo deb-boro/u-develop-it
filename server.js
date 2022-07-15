5dfa774619270c6072c2d5557b438a4bff28066b
deb boro <debajit.boro666@gmail.com>
2022-07-14 23:06:39 -0400

    post delete

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const inputCheck = require('./utils/inputCheck')
 const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001
@@ -27,6 +28,85 @@ app.get('/', (req, res) => {
   })
 })
 
+//get all candidate
+app.get('/api/candidates', (req, res) => {
+  const sql = `select * from candidates`
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message })
+      return
+    } else {
+      res.json({
+        message: 'success',
+        data: rows,
+      })
+    }
+    console.log(rows)
+  })
+})
+
+//delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id =?`
+  const params = [req.params.id]
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message })
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'candidate not found',
+      })
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id,
+      })
+    }
+  })
+})
+//create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected',
+  )
+  const sql =
+    'INSERT INTO candidates (first_name, last_name, industry_connected) VALUES(?,?,?)'
+  const params = [body.first_name, body.last_name, body.industry_connected]
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+      return
+    }
+    res.json({
+      message: 'success',
+      data: body,
+    })
+  })
+})
+
+//get a single candidate
+// app.get('/api/candidate/:id', (req, res) => {
+//   const sql = `select * from candidates where id= ?`
+//   const params = [req.params.id]
+
+//   db.query(sql, params, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message })
+//       return
+//     } else {
+//       res.json({
+//         message: 'success',
+//         data: rows,
+//       })
+//     }
+//     console.log(rows)
+//   })
+// })
 // db.query(`SELECT * FROM candidates`, (err, rows) => {
 //   console.log(rows)
 // })
